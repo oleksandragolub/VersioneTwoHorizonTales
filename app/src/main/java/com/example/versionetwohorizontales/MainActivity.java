89bae6390d7a49a51355dd9ce89b53919c4bc0d8
GolubOleksandra <102955072+GolubOleksandra@users.noreply.github.com>
2024-09-06 16:57:48 +0200

    First commit

---
+++ app/src/main/java/com/example/versionetwohorizontales/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.versionetwohorizontales;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate(): ");
+    }
+}
